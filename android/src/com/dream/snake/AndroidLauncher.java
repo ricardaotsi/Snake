35f2ace30ec65d3c1525a3c2a76e0ad68ebde5db
Ricardo Arruda Sowek <ricardaotsi@gmail.com>
2016-11-16 10:18:46 -0200

    Projeto criado

---
+++ android/src/com/dream/snake/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.dream.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dream.snake.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new Game(), config);
+	}
+}
